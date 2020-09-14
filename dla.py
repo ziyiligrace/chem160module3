cf77e5fb8c87f8a9b399d3fd1730cf964c051d83
ziyiligrace <69704067+ziyiligrace@users.noreply.github.com>
2020-09-13 18:32:48 -0700

    Add files via upload

---
+++ dla.py
@@ -0,0 +1,32 @@
+from random import choice
+from drawgridlib import drawgrid
+npart=300
+side=75  #Should be an odd number
+steps = [(1,0),(-1,0),(0,1),(0,-1)]
+grid=[[0 for x in range(side)] for y in range(side)]
+grid[side//2][side//2]=1
+
+for ipart in range (npart) :
+    #start particle at origin
+    x,y = 0,0
+    # perform the random walk until particle aggregates
+    while 1:
+        grid[x][y]=0 #remove particle from  current spot
+        #randomly move particle
+        sx,sy = choice(steps)
+        x += sx
+        y += sy
+        #enforce periodic boundaries
+        if x<0: x=side-1
+        if y<0: y=side-1
+        if x==side: x=0
+        if y==side: y=0
+        grid[x][y]=1 #put particle in new location
+
+
+
+        # Stop if you are next to a particle
+        if (grid[(x+1)%side][y]+grid[x][(y+1)%side]+
+            grid[(x+side-1)%side][y]+grid[x][(y+side-1)%side])>0:
+            break
+drawgrid(grid,side)
